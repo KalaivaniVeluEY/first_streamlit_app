f6f98309c7d9a59f3844217c7fe08def814ef2b6
Kalai <63645471+KalaivaniVeluEY@users.noreply.github.com>
2022-11-30 11:31:18 +0000

    Update streamlit_app.py
    Fixed the typo on line 15

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,6 @@ import pandas
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
 #Lets add a pick list to pick the fruits needed for the smootie
-steamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 streamlit.dataframe(my_fruit_list)
