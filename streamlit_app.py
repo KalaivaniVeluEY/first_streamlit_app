7d959734c94c123ce010d1e70fd0b7a0ecb12319
Kalai <63645471+KalaivaniVeluEY@users.noreply.github.com>
2022-11-30 11:30:01 +0000

    Update streamlit_app.py
    Add a smoothie header and a new picklist from where fruits can be chosen.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,13 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado toast')
+streamlit.header('Let me make my own smoothie')
 
 #Lets import a csv file and view it
 import pandas
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+#Lets add a pick list to pick the fruits needed for the smootie
+steamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)
