0b44d65c92746c36bf5c2a8c3575d29b2ba1a8a7
Kalai <63645471+KalaivaniVeluEY@users.noreply.github.com>
2022-11-30 11:32:56 +0000

    Update streamlit_app.py
    Display the fruits instead of numbers

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ streamlit.header('Let me make my own smoothie')
 #Lets import a csv file and view it
 import pandas
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Lets add a pick list to pick the fruits needed for the smootie
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
